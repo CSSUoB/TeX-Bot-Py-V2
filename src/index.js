a586a0cffd512a81e905521ee05e1109f7aecf40
sapphyree <sonexix@gmail.com>
2022-07-31 00:12:47 +0100

    Add command handler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import env from 'dotenv';
-
-env.config();
+const fs = require('fs');
+const path = require('path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+require('../config.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (err) {
+		console.error(err);
+		await interaction.reply({
+			content: 'There was an errour while executing this command.',
+			ephemeral: true,
+		});
+	}
 });
 
 client.login();
