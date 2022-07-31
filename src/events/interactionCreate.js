fa5b95add78e169a2646274e25d8fb7377fa4fee
sapphyree <sonexix@gmail.com>
2022-07-31 01:48:42 +0100

    Fix formatting

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,27 +1,24 @@
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(
-			`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`,
-		);
+  name: "interactionCreate",
+  execute(interaction) {
+    console.log(
+      `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
+    );
 
-		if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-		const command = interaction.client.commands.get(
-			interaction.commandName,
-		);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) return;
+    if (!command) return;
 
-		try {
-			command.execute(interaction);
-		}
-		catch (err) {
-			console.error(err);
-			interaction.reply({
-				content: 'There was an errour while executing this command.',
-				ephemeral: true,
-			});
-		}
-	},
+    try {
+      command.execute(interaction);
+    } catch (err) {
+      console.error(err);
+      interaction.reply({
+        content: "There was an errour while executing this command.",
+        ephemeral: true,
+      });
+    }
+  },
 };
