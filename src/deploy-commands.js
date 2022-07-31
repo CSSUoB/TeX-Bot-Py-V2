a5b102fdfb4d14ad052b86c0193014e292df8058
sapphyree <sonexix@gmail.com>
2022-07-31 01:11:37 +0100

    Relocate config files and fix formatting

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,27 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-const { Routes } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-require('../config.js');
+const fs = require("fs");
+const path = require("path");
+const { Routes } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+require("../config/config.js");
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith('.js'));
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	console.log(command);
-	commands.push(command.data.toJSON());
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  console.log(command);
+  commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(
-	Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
-	{ body: commands },
-)
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+rest
+  .put(
+    Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
+    { body: commands }
+  )
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-const fs = require('fs');
-const path = require('path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-require('../config.js');
+const fs = require("fs");
+const path = require("path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+require("../config/config.js");
 
 const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith('.js'));
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
 
-	client.commands.set(command.data.name, command);
+  client.commands.set(command.data.name, command);
 }
 
-const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
-	.readdirSync(eventsPath)
-	.filter((file) => file.endsWith('.js'));
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 client.login();
