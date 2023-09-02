25a87847d5b4e4b867d0d33e763f46165111003e
CarrotManMatt <matt@carrotmanmatt.com>
2023-09-02 23:30:08 +0100

    Update _induct function to send an extra message about memberships

--- cogs/events.py
+++ cogs/events.py
@@ -130,6 +130,9 @@ class Events_Cog(TeXBotCog):
             await after.send(
                 f"**Congrats on joining the CSS Discord server as a guest!** You now have access to contribute to all the public channels.\n\nSome things to do to get started:\n1. Check out our rules in {welcome_channel_mention}\n2. Head to {roles_channel_mention} and click on the icons to get optional roles like pronouns and year groups\n3. Change your nickname to whatever you wish others to refer to you as (You can do this by right-clicking your name in the members list to the right & selecting \"Edit Server Profile\")"
             )
+            await after.send(
+                "You can also get yourself an annual membership to CSS for only £5! Just head to https://cssbham.com/join. You'll get awesome perks like a free T-shirt:shirt:, access to member only events:calender_spiral: & a cool green name on the CSS Discord server:green_square:! Checkout all the perks at https://cssbham.com/membership."
+            )
 
     @TeXBotCog.listener()
     async def on_member_leave(self, member: discord.Member) -> None:
