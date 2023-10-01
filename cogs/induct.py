7ded34719cb6a6e9c69c393cdbd042ad229529b6
CarrotManMatt <matt@carrotmanmatt.com>
2023-10-01 04:05:27 +0100

    Rename welcome channel shortcut accessor to "rules_channel"

--- cogs/induct.py
+++ cogs/induct.py
@@ -59,10 +59,10 @@ class InductSendMessageCog(TeXBotCog):
                         reason="Delete introduction reminders after member is inducted."
                     )
 
-            welcome_channel_mention: str = "`#welcome`"
-            welcome_channel: discord.TextChannel | None = await self.bot.welcome_channel
-            if welcome_channel:
-                welcome_channel_mention = welcome_channel.mention
+            rules_channel_mention: str = "`#welcome`"
+            rules_channel: discord.TextChannel | None = await self.bot.rules_channel
+            if rules_channel:
+                rules_channel_mention = rules_channel.mention
 
             roles_channel_mention: str = "`#roles`"
             roles_channel: discord.TextChannel | None = await self.bot.roles_channel
@@ -79,7 +79,7 @@ class InductSendMessageCog(TeXBotCog):
                 f"**Congrats on joining the CSS Discord server as a {user_type}!**"
                 " You now have access to contribute to all the public channels."
                 "\n\nSome things to do to get started:"
-                f"\n1. Check out our rules in {welcome_channel_mention}"
+                f"\n1. Check out our rules in {rules_channel_mention}"
                 f"\n2. Head to {roles_channel_mention} and click on the icons to get"
                 " optional roles like pronouns and year groups"
                 "\n3. Change your nickname to whatever you wish others to refer to you as"
