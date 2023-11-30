cf8c829cfa3d1b661102d962c65aee98e65bc688
CarrotManMatt <matt@carrotmanmatt.com>
2023-11-30 19:54:23 +0000

    Fix incorrect newlines

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ then ensures the Django database is correctly migrated to the latest version and
 the asynchronous running process for the Discord bot.
 """
 
-
 import discord
 from django.core import management
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -39,6 +39,7 @@ class TestGenerateInviteURL:
             invite_url
         )
 
+
 # TODO(CarrotManMatt): Move to stats_tests  # noqa: FIX002
 # https://github.com/CSSUoB/TeX-Bot-Py-V2/issues/57
 # class TestPlotBarChart:
