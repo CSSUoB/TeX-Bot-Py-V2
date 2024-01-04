dd1fb89bd2033fe309ae947644520e66f92cdb04
CarrotManMatt <matt@carrotmanmatt.com>
2024-01-04 23:10:37 +0000

    Split Env variables setup into a single method per variable

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,13 @@
 """Contains the entire package required to run Django's ORM as a database connector."""
+
+import os
+
+import django
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "db._settings"
+e: RuntimeError
+try:
+    django.setup()
+except RuntimeError as e:
+    if "populate() isn't reentrant" not in str(e):
+        raise
