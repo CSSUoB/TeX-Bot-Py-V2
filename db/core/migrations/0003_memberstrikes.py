5a4631ea931892b9e6754b98e92ed242adc4caa8
CarrotManMatt <matt@carrotmanmatt.com>
2024-01-18 18:56:17 +0000

    Allow unchanged files

--- db/core/migrations/0003_memberstrikes.py
+++ db/core/migrations/0003_memberstrikes.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('hashed_member_id', models.CharField(max_length=64, unique=True, validators=[django.core.validators.RegexValidator('\\A[A-Fa-f0-9]{64}\\Z', 'hashed_member_id must be a valid sha256 hex-digest.')], verbose_name='Hashed Discord Member ID')),
-                ('strikes', models.PositiveIntegerField(validators=[django.core.validators.MinValueValidator(0)], verbose_name='Number of strikes')),
+                ('strikes', models.PositiveIntegerField(blank=True, default=0, validators=[django.core.validators.MinValueValidator(0)], verbose_name='Number of strikes')),
             ],
             options={
                 'verbose_name': 'Hashed Discord ID of Member that has been previously given one or more strikes because they broke one or more of the CSS Discord server rules',
