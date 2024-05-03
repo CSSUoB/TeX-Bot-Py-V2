2d01e9eaf5006648d9848c5fa4fffba5b51077c5
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-05-03 23:00:45 +0100

    return member instead on both

--- cogs/send_get_roles_reminders.py
+++ cogs/send_get_roles_reminders.py
@@ -161,7 +161,7 @@ class SendGetRolesRemindersTaskCog(TeXBotBaseCog):
                         "Member: %s left the server before the introduction "
                         "reminder could be sent.",
                     ), member
-                    repr(member.id)
+                    repr(member)
 
             await member.send(
                 "Hey! It seems like you joined "
