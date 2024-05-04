f9e44dde89a5408f6eae89e5fe64c5001a25d04c
MattyThehacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-05-04 11:28:54 -0400

    fix bad syntax

--- cogs/kill.py
+++ cogs/kill.py
@@ -39,7 +39,7 @@ class KillCommandCog(TeXBotBaseCog):
 
     async def confirm_kill(self, ctx: TeXBotApplicationContext) -> None:
         """Confirm that the user did indeed intent to kill the bot."""
-        confirmation_message_channel: discord.TextChannel = discord.Guild.get_channel(1128049192498102483) # noqa: E501
+        confirmation_message_channel: discord.TextChannel = discord.Guild.get_channel(self, 1128049192498102483) # noqa: E501
         committee_role: discord.Role = await self.bot.committee_role
 
         confirmation_message: discord.Message = await confirmation_message_channel.send(
