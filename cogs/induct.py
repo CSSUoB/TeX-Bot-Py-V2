a726e7f05eee21a2dd3d1ab46012f5a9024da8ad
MattyThehacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-05-13 15:40:32 -0400

    shut up mypy

--- cogs/induct.py
+++ cogs/induct.py
@@ -351,7 +351,7 @@ class InductUserCommandsCog(BaseInductCog):
         """
         await self._perform_induction(ctx, member, silent=True)
 
-    @discord.message_command(name="Induct Message Author")
+    @discord.message_command(name="Induct Message Author")  # type: ignore[no-untyped-call, misc]
     @CommandChecks.check_interaction_user_has_committee_role
     @CommandChecks.check_interaction_user_in_main_guild
     async def non_silent_message_induct(self, ctx: TeXBotApplicationContext, message: discord.Message) -> None: # noqa: E501
@@ -361,9 +361,10 @@ class InductUserCommandsCog(BaseInductCog):
         The non_silent_message_induct command executes the same process as the
         induct slash command using the message context menu instead of the user menu.
         """
-        await self._perform_induction(ctx, message.author, silent=False)
+        author: discord.Member = message.author
+        await self._perform_induction(ctx, author, silent=False)
 
-    @discord.message_command(name="Silent Induct Message Author")
+    @discord.message_command(name="Silent Induct Message Author")  # type: ignore[no-untyped-call, misc]
     @CommandChecks.check_interaction_user_has_committee_role
     @CommandChecks.check_interaction_user_in_main_guild
     async def silent_message_induct(self, ctx: TeXBotApplicationContext, message: discord.Message) -> None: # noqa: E501
