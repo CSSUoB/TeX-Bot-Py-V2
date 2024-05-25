48bc77f7aea760e970c59997af777720ce961dbf
MattyThehacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-05-25 11:52:31 -0400

    more line lengths

--- cogs/send_get_roles_reminders.py
+++ cogs/send_get_roles_reminders.py
@@ -172,9 +172,11 @@ class SendGetRolesRemindersTaskCog(TeXBotBaseCog):
                     "and click on the icons to get optional roles like pronouns "
                     "and year group identifiers.",
                 )
-            except discord.Forbidden as e:
-                logger.warning("Failed to open DM channel to %s so no reminder was sent.", member)
-                logger.debug(e.text)
+            except discord.Forbidden:
+                logger.warning(
+                    "Failed to open DM channel to user, %s, so no role reminder was sent.",
+                    member,
+                )
 
             await SentGetRolesReminderMember.objects.acreate(discord_id=member.id)
 
