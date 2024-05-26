2e4808190cde47fc7dd17580719572f0fdf93823
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-05-26 23:06:04 +0100

    add strike exceptions

---
+++ exceptions/strike.py
@@ -0,0 +1,42 @@
+"""Custom exceptions for strike related cogs."""
+
+from collections.abc import Sequence
+
+__all__: Sequence[str] = (
+    "StrikeTrackingError",
+    "NoAuditLogsStrikeTrackingError",
+)
+
+from base import BaseTeXBotError
+from classproperties import classproperty
+
+
+class StrikeTrackingError(BaseTeXBotError, RuntimeError):
+    """
+    Exception class to raise when any error occurs while tracking moderation actions.
+
+    If this error occurs, it is likely that manually applied moderation actions will be missed
+    and not tracked correctly.
+    """
+
+    # noinspection PyMethodParameters,PyPep8Naming
+    @classproperty
+    def DEFAULT_MESSAGE(cls) -> str:  # noqa: N802,N805
+        """The message to be displayed alongside this exception class if none is provided."""  # noqa: D401
+        return "An error occurred while trying to track manually applied moderation actions."
+
+
+class NoAuditLogsStrikeTrackingError(BaseTeXBotError, RuntimeError):
+    """
+    Exception class to raise when there are no audit logs to resolve the committee member.
+
+    If this error occurs, it is likely that manually applied moderation actions will be missed
+    and not tracked correctly.
+    """
+
+    # noinspection PyMethodParameters,PyPep8Naming
+    @classproperty
+    def DEFAULT_MESSAGE(cls) -> str:  # noqa: N802,N805
+        """The message to be displayed alongside this exception class if none is provided."""  # noqa: D401
+        return "Unable to retrieve audit log entry after possible manual moderation action."
+
