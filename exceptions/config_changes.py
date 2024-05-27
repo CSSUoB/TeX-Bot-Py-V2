2a26a968d8fa614a8142c5385750e64c07476619
Matt Norton <matt@carrotmanmatt.com>
2024-05-27 03:15:43 +0100

    Remove re-added exceptions after merge

--- exceptions/config_changes.py
+++ exceptions/config_changes.py
@@ -2,14 +2,25 @@
 
 from collections.abc import Sequence
 
-__all__: Sequence[str] = (
-    "ImproperlyConfiguredError",
-    "BotRequiresRestartAfterConfigChange",
-)
+__all__: Sequence[str] = ("BotRequiresRestartAfterConfigChange",)
 
 
-class ImproperlyConfiguredError(Exception):
-    """Exception class to raise when environment variables are not correctly provided."""
+from .base import BaseTeXBotError
 
-class BotRequiresRestartAfterConfigChange(Exception):
-    """Exception class to raise when the bot requires a reboot to apply changes."""
+from classproperties import classproperty
+
+
+class BotRequiresRestartAfterConfigChange(BaseTeXBotError):
+    """Exception class to raise to enforce handling of bot restarts after config changes."""
+
+    # noinspection PyMethodParameters,PyPep8Naming
+    @classproperty
+    def DEFAULT_MESSAGE(cls) -> str:  # noqa: N802,N805
+        """The message to be displayed alongside this exception class if none is provided."""  # noqa: D401
+        return "TeX-Bot requires a restart due to configuration changes."
+
+    def __init__(self, message: str | None = None, changed_settings: set[str] | None = None) -> None:  # noqa: E501
+        """Initialize a ValueError exception for a non-existent user ID."""
+        self.changed_settings: set[str] | None = changed_settings
+
+        super().__init__(message)
