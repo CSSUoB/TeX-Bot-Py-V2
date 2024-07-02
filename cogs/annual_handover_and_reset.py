7d0f50fe5d737c5dffe5bd19f47b1d965e6fc92c
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-07-02 19:17:19 +0100

    eeeeeeeat deez nuts

--- cogs/annual_handover_and_reset.py
+++ cogs/annual_handover_and_reset.py
@@ -30,7 +30,7 @@ class CommitteeHandoverCommandCog(TeXBotBaseCog):
         """
         Definition & callback response of the "committeee_handover" command.
 
-        The "committeee_handover" command runs the relevant handover methods
+        The "committee_handover" command runs the relevant handover methods
         which will perform the following actions:
         - Give the "Committee" role to any users that have the "Committee-Elect" role
         - Remove the "Committee-Elect" role from any user that has it
