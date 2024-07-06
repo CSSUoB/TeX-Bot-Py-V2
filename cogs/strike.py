59b8c8ceb341490bf3e9bcd27dc32005e710b34c
Matt Norton <matt@carrotmanmatt.com>
2024-07-06 19:23:16 +0100

    Fix not checking user in strike audit log filtering

--- cogs/strike.py
+++ cogs/strike.py
@@ -701,10 +701,13 @@ class ManualModerationCog(BaseStrikeCog):
 
         audit_log_entry: discord.AuditLogEntry
         async for audit_log_entry in main_guild.audit_logs(limit=5):
-            AUDIT_LOG_ENTRY_IS_AUTOMOD_ACTION: bool = audit_log_entry.action == (
-                discord.AuditLogAction.auto_moderation_user_communication_disabled
+            FOUND_CORRECT_AUDIT_LOG_ENTRY: bool = (
+                audit_log_entry.target == after
+                and audit_log_entry.action == (
+                    discord.AuditLogAction.auto_moderation_user_communication_disabled
+                )
             )
-            if AUDIT_LOG_ENTRY_IS_AUTOMOD_ACTION:
+            if FOUND_CORRECT_AUDIT_LOG_ENTRY:
                 await self._confirm_manual_add_strike(
                     strike_user=after,
                     action=audit_log_entry.action,
