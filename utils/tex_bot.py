04227bf4bef95e5d743eddb6dceab83c42152449
Matt Norton <matt@carrotmanmatt.com>
2024-07-14 16:09:51 +0100

    Fix linting error

--- utils/tex_bot.py
+++ utils/tex_bot.py
@@ -92,7 +92,7 @@ class TeXBot(discord.Bot):
         """
         MAIN_GUILD_EXISTS: Final[bool] = bool(
             self._main_guild
-            and self._tex_bot_has_guild(settings["_DISCORD_MAIN_GUILD_ID"])
+            and self._tex_bot_has_guild(settings["_DISCORD_MAIN_GUILD_ID"])  # noqa: COM812
         )
         if not MAIN_GUILD_EXISTS:
             raise GuildDoesNotExistError(guild_id=settings["_DISCORD_MAIN_GUILD_ID"])
