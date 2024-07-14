4e9acec9391f3798739dc0cdb17bb121768b1569
Matt Norton <matt@carrotmanmatt.com>
2024-07-14 16:08:29 +0100

    Fix missed spelling mistakes

--- exceptions/messages.py
+++ exceptions/messages.py
@@ -27,7 +27,7 @@ class InvalidMessagesJSONFileError(ImproperlyConfiguredError):
 
     @override
     def __init__(self, message: str | None = None, dict_key: str | None = None) -> None:
-        """Initialize an ImproperlyConfigured exception for an invalid messages.json file."""
+        """Initialise an ImproperlyConfigured exception for an invalid messages.json file."""
         self.dict_key: str | None = dict_key
 
         super().__init__(message)
@@ -44,7 +44,7 @@ class MessagesJSONFileMissingKeyError(InvalidMessagesJSONFileError):
 
     @override
     def __init__(self, message: str | None = None, missing_key: str | None = None) -> None:
-        """Initialize a new InvalidMessagesJSONFile exception for a missing key."""
+        """Initialise a new InvalidMessagesJSONFile exception for a missing key."""
         super().__init__(message, dict_key=missing_key)
 
     @property
@@ -68,7 +68,7 @@ class MessagesJSONFileValueError(InvalidMessagesJSONFileError):
 
     @override
     def __init__(self, message: str | None = None, dict_key: str | None = None, invalid_value: object | None = None) -> None:  # noqa: E501
-        """Initialize a new InvalidMessagesJSONFile exception for a key's invalid value."""
+        """Initialise a new InvalidMessagesJSONFile exception for a key's invalid value."""
         self.invalid_value: object | None = invalid_value
 
         super().__init__(message, dict_key)
