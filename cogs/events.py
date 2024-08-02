572e03824772b28b065a623b7b14fadc59f4f1eb
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2024-08-02 22:43:28 +0000

    pending

--- cogs/events.py
+++ cogs/events.py
@@ -7,11 +7,13 @@ __all__: Sequence[str] = ("EventsManagementCommandsCog",)
 
 import logging
 from collections.abc import Mapping
+from datetime import datetime
 from logging import Logger
 from typing import Final
 
 import aiohttp
 import bs4
+import dateutil
 import discord
 from bs4 import BeautifulSoup
 
@@ -41,14 +43,15 @@ EVENT_TABLE_ID: Final[str] = "ctl00_ctl00_Main_AdminPageContent_gvEvents"
 class EventsManagementCommandsCog(TeXBotBaseCog):
     """Cog class to define event management commands."""
 
-    async def _get_all_guild_events(self, ctx: TeXBotApplicationContext) -> None:
+    async def _get_all_guild_events(self, ctx: TeXBotApplicationContext, from_date: str, to_date: str) -> None:  # noqa: E501
         """Fetch all events on the guild website."""
         form_data: dict[str, str] = {
-            FROM_DATE_KEY: "01/01/2024",
-            TO_DATE_KEY: "01/01/2025",
+            FROM_DATE_KEY: from_date,
+            TO_DATE_KEY: to_date,
             BUTTON_KEY: "Find Events",
             "__EVENTTARGET": "",
             "__EVENTARGUMENT": "",
+            "__VIEWSTATEENCRYPTED": "",
         }
         http_session: aiohttp.ClientSession = aiohttp.ClientSession(
             headers=REQUEST_HEADERS,
@@ -58,19 +61,42 @@ class EventsManagementCommandsCog(TeXBotBaseCog):
             data_response = BeautifulSoup(await field_data.text(), "html.parser")
             view_state: str = data_response.find("input", {"name": "__VIEWSTATE"}).get("value")  # type: ignore[union-attr, assignment]
             event_validation: str = data_response.find("input", {"name": "__EVENTVALIDATION"}).get("value")  # type: ignore[union-attr, assignment]  # noqa: E501
+            view_state_generator: str = data_response.find("input", {"name": "__VIEWSTATEGENERATOR"}).get("value")  # type: ignore[union-attr, assignment]  # noqa: E501
 
         form_data["__VIEWSTATE"] = view_state
         form_data["__EVENTVALIDATION"] = event_validation
+        form_data["__VIEWSTATEGENERATOR"] = view_state_generator
+
+        new_cookies: dict[str, str] = {
+            ".ASPXAUTH": settings["MEMBERS_LIST_AUTH_SESSION_COOKIE"],
+        }
+
+        anti_xss_cookie = field_data.cookies.get("__AntiXsrfToken")
+        if anti_xss_cookie is not None:
+            new_cookies["__AntiXsrfToken"] = anti_xss_cookie.value
+
+        asp_net_shared_cookie = field_data.cookies.get(".AspNet.SharedCookie")
+        if asp_net_shared_cookie is not None:
+            new_cookies[".AspNet.SharedCookie"] = asp_net_shared_cookie.value
+
+        asp_session_id = field_data.cookies.get("ASP.NET_SessionId")
+        if asp_session_id is not None:
+            new_cookies["ASP.NET_SessionId"] = asp_session_id.value
 
         session_v2: aiohttp.ClientSession = aiohttp.ClientSession(
             headers=REQUEST_HEADERS,
-            cookies=REQUEST_COOKIES,
+            cookies=new_cookies,
         )
         async with session_v2, session_v2.post(REQUEST_URL, data=form_data) as http_response:
-            if http_response.headers.get("Content-Type") == "gzip":
-                response_html = await http_response.read()
-            else:
-                response_html = await http_response.text()
+            if http_response.status != 200:
+                await self.command_send_error(
+                    ctx,
+                    message="Returned a non-200 status code!!!.",
+                )
+                logger.debug(http_response)
+                return
+
+            response_html: str = await http_response.text()
 
         parsed_html: bs4.Tag | bs4.NavigableString | None = BeautifulSoup(
                 response_html,
@@ -93,16 +119,43 @@ class EventsManagementCommandsCog(TeXBotBaseCog):
             logger.debug(http_response.request_info)
             return
 
-        await ctx.respond(parsed_html)
+        event_list: list[bs4.Tag] = parsed_html.find_all("tr")
+
+        event_list.pop(0)
+
+        event_ids: dict[str, str] = {
+            event.find("a").get("href").split("/")[5]: event.find("a").text  # type: ignore[union-attr]
+            for event in event_list
+        }
+
+        await ctx.respond(event_ids)
+
 
     @discord.slash_command(  # type: ignore[no-untyped-call, misc]
         name="get-events",
         description="Returns all events currently on the guild website.",
     )
+    @discord.option(  # type: ignore[no-untyped-call, misc]
+        name="from-date",
+        description="The date to start searching from.",
+        required=False,
+        input_type=str,
+        parameter_name="str_from_date",
+    )
+    @discord.option(  # type: ignore[no-untyped-call, misc]
+        name="to-date",
+        description="The date to stop searching at.",
+        required=False,
+        input_type=str,
+        parameter_name="str_to_date",
+    )
     @CommandChecks.check_interaction_user_has_committee_role
     @CommandChecks.check_interaction_user_in_main_guild
-    async def get_events(self, ctx: TeXBotApplicationContext) -> None:
+    async def get_events(self, ctx: TeXBotApplicationContext, *, str_from_date: str, str_to_date: str) -> None:  # noqa: E501
         """Command to get the events on the guild website."""
+        from_date_dt = datetime.strptime(str_from_date)
+
+
         await self._get_all_guild_events(ctx)
 
 
