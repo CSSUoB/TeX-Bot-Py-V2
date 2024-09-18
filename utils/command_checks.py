279ebed28e0fc13af908692d898e9f8e094fb054
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2024-09-18 08:54:07 +0100

    New line ting init

--- utils/command_checks.py
+++ utils/command_checks.py
@@ -85,3 +85,4 @@ CommandChecks.check_interaction_user_in_main_guild = commands.check_any(
 # noinspection PyProtectedMember
 CommandChecks.check_interaction_user_has_committee_role = commands.check_any(commands.check(CommandChecks._check_interaction_user_has_committee_role),  # type: ignore[arg-type] # noqa: SLF001
 )
+
