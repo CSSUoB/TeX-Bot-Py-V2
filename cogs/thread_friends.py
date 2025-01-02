5d315fc061c9161095426b8091d0bd12b05c5cc8
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2025-01-02 00:32:57 +0000

    refactor to make 2 separate commands

--- cogs/thread_friends.py
+++ cogs/thread_friends.py
@@ -28,35 +28,31 @@ class AddUsersToThreadsCog(TeXBotBaseCog):
     """Cog for adding users to threads."""
 
     @staticmethod
-    async def autocomplete_get_members_and_roles(ctx: "TeXBotAutocompleteContext") -> "AbstractSet[discord.OptionChoice] | AbstractSet[str]":  # noqa: E501
-        """
-        Autocomplete callable that generates the set of available selectable members.
-
-        This list of selectable members is used in any of the "induct" slash-command options
-        that have a member input-type.
-        """
+    async def autocomplete_get_members(ctx: "TeXBotAutocompleteContext") -> "AbstractSet[discord.OptionChoice] | AbstractSet[str]":  # noqa: E501
+        """Autocomplete callable that generates the set of available selectable members."""
         try:
             main_guild: discord.Guild = ctx.bot.main_guild
             guest_role: discord.Role = await ctx.bot.guest_role
         except (GuildDoesNotExistError, GuestRoleDoesNotExistError):
             return set()
 
-        members: set[discord.Member | discord.Role] = {
-            member
+        return {
+            discord.OptionChoice(name=member.name, value=str(member.id))
             for member in main_guild.members
             if not member.bot and guest_role in member.roles
         }
 
-        members.update(main_guild.roles)
-
-        if not ctx.value or ctx.value.startswith("@"):
-            return {
-                discord.OptionChoice(name=f"@{member.name}", value=str(member.id))
-                for member in members
-            }
+    @staticmethod
+    async def autocomplete_get_roles(ctx: "TeXBotAutocompleteContext") -> "AbstractSet[discord.OptionChoice] | AbstractSet[str]":  # noqa: E501
+        """Autocomplete callable that generates the set of available selectable roles."""
+        try:
+            main_guild: discord.Guild = ctx.bot.main_guild
+        except GuildDoesNotExistError:
+            return set()
 
         return {
-            discord.OptionChoice(name=member.name, value=str(member.id)) for member in members
+            discord.OptionChoice(name=role.name, value=str(role.id))
+            for role in main_guild.roles
         }
 
     async def add_user_or_role_silently(self, user_or_role: discord.Member | discord.Role, thread: discord.Thread) -> None:  # noqa: E501
@@ -119,10 +115,10 @@ class AddUsersToThreadsCog(TeXBotBaseCog):
         description="Adds selected users to a thread.",
     )
     @discord.option(  # type: ignore[no-untyped-call, misc]
-        name="user_or_role",
-        description="The user or role to add to the thread.",
+        name="user",
+        description="The user to add to the thread.",
         input_type=str,
-        autocomplete=discord.utils.basic_autocomplete(autocomplete_get_members_and_roles),  # type: ignore[arg-type]
+        autocomplete=discord.utils.basic_autocomplete(autocomplete_get_members),  # type: ignore[arg-type]
         required=True,
         parameter_name="user_or_role",
     )
@@ -133,7 +129,54 @@ class AddUsersToThreadsCog(TeXBotBaseCog):
         required=False,
         parameter_name="silent",
     )
-    async def add_users_to_thread(self, ctx: "TeXBotApplicationContext", user_or_role: str, silent: bool) -> None:  # noqa: E501, FBT001
+    async def add_user_to_thread(self, ctx: "TeXBotApplicationContext", user_id_str: str, silent: bool) -> None:  # noqa: E501, FBT001
+        """Add users or roles to a thread."""
+        if not isinstance(ctx.channel, discord.Thread):
+            await self.command_send_error(
+                ctx=ctx,
+                message="This command can only be used in a thread.",
+            )
+            return
+
+        thread: discord.Thread = ctx.channel
+
+        try:
+            user_to_add: discord.Member = await self.bot.get_member_from_str_id(user_id_str)
+        except ValueError:
+            logger.debug("User ID: %s is not a valid ID.", user_id_str)
+            await ctx.respond(content=f"The user: {user_id_str} is not valid.")
+            return
+
+        if silent:
+            await self.add_user_or_role_silently(user_to_add, thread)
+        else:
+            await self.add_user_or_role_with_ping(user_to_add, thread)
+
+        await ctx.respond(
+            content=f"User {user_to_add.mention} has been added to the thread.",
+            ephemeral=True,
+        )
+
+    @discord.slash_command(  # type: ignore[no-untyped-call, misc]
+        name="add_role_to_thread",
+        description="Adds the selected role and it's users to a thread.",
+    )
+    @discord.option(  # type: ignore[no-untyped-call, misc]
+        name="role",
+        description="The role to add to the thread.",
+        input_type=str,
+        autocomplete=discord.utils.basic_autocomplete(autocomplete_get_roles),  # type: ignore[arg-type]
+        required=True,
+        parameter_name="user_or_role",
+    )
+    @discord.option(  # type: ignore[no-untyped-call, misc]
+        name="silent",
+        description="Whether the users being added should be pinged or not.",
+        input_type=bool,
+        required=False,
+        parameter_name="silent",
+    )
+    async def add_role_to_thread(self, ctx: "TeXBotApplicationContext", user_or_role: str, silent: bool) -> None:  # noqa: E501, FBT001
         """Add users or roles to a thread."""
         main_guild: discord.Guild = ctx.bot.main_guild
 
@@ -147,32 +190,17 @@ class AddUsersToThreadsCog(TeXBotBaseCog):
         thread: discord.Thread = ctx.channel
 
         try:
-            user_object: discord.Member = await self.bot.get_member_from_str_id(user_or_role)
-        except ValueError:
-            pass
-        else:
-            if silent:
-                await self.add_user_or_role_silently(user_object, thread)
-            else:
-                await self.add_user_or_role_with_ping(user_object, thread)
-
-            await ctx.respond(
-                content=f"User {user_object.mention} has been added to the thread."
-            )
-            return
-
-        try:
             role_id: int = int(user_or_role)
         except ValueError:
-            logger.debug("Role or User ID: %s is not a valid ID.", user_or_role)
-            await ctx.respond(content=f"The role or user: {user_or_role} is not valid.")
+            logger.debug("Role ID: %s is not a valid ID.", user_or_role)
+            await ctx.respond(content=f"The role: {user_or_role} is not valid.")
             return
 
         role_object: discord.Role | None = discord.utils.get(main_guild.roles, id=role_id)
         if role_object is None:
             await self.command_send_error(
                 ctx=ctx,
-                message=f"The role or user: <@{role_id}> is not valid or couldn't be found.",
+                message=f"The role: <@{role_id}> is not valid or couldn't be found.",
             )
             return
 
@@ -182,5 +210,6 @@ class AddUsersToThreadsCog(TeXBotBaseCog):
             await self.add_user_or_role_with_ping(role_object, thread)
 
         await ctx.respond(
-            content=f"Role {role_object.mention} has been added to the thread."
+            content=f"Role {role_object.mention} has been added to the thread.",
+            ephemeral=True,
         )
