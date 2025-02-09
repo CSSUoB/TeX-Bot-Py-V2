40c4eeb162d770374761cfd390524eef5adddba9
Matt Norton <matt@carrotmanmatt.com>
2025-02-09 02:13:51 +0000

    Add function definition fixes

--- cogs/everest.py
+++ cogs/everest.py
@@ -19,23 +19,50 @@ __all__: "Sequence[str]" = ("EverestCommandCog",)
 class EverestCommandCog(TeXBotBaseCog):
     """Cog class that defines the "/everest" command and its call-back method."""
 
-    @client.tree.command(description="How many steps of everest is your assignment worth?") # The description of the command
+    @staticmethod
+    async def autocomplete_get_course_types(
+        ctx: "TeXBotAutocompleteContext",
+    ) -> "AbstractSet[discord.OptionChoice] | AbstractSet[str]":
+        raise NotImplementedError  # TODO(Donian960)
+
+    @discord.slash_command(description="How many steps of everest is your assignment worth?")
     @app_commands.describe(
         course="What course are you on (bsc or msci)",
         year="Current year of the course (1 to 4)",
         value="What % of a module is the assignment worth"
-    ) # The descriptions of the attributes of the command
-    async def everest(interaction: discord.Interaction, course: str, year: int, value: float): # defining function
+    )
+    @discord.option(  # type: ignore[no-untyped-call, misc]
+        name="course",
+        description="Your type of university course: B.Sc. or M.Sci.",
+        input_type=...,  # TODO(Donian960): Make type into a limited set of choices
+        autocomplete=discord.utils.basic_autocomplete(autocomplete_get_course_types),  # type: ignore[arg-type]
+        required=True,
+        parameter_name="course_type",
+    )
+    @discord.option(  # type: ignore[no-untyped-call, misc]
+        name="year",
+        description="Current year of the course (1 to 4).",
+        input_type=...,  # TODO(Donian960): Make type into a limited set of choices
+        required=True,
+        parameter_name="current_course_year",
+    )
+    @discord.option(  # type: ignore[no-untyped-call, misc]
+        name="module_percentage",
+        description="The percentage of a module that the assignment worth.",
+        input_type=float,  # TODO(Donian960): limit values to between 0% & 100%
+        required=True,
+    )
+    async def everest(self, ctx: "TeXBotApplicationContext", course_type: ..., current_course_year: int, module_percentage: float):  # TODO(Donian960): Choose correct type for 'course_type' variable
         message_start = "Course: " + str(course) + ", Year: " + str(year) + ", Value: " +str(value) + "%\n" # message data
 
         course = course.lower() # allowing capital courses
-        
+
         if course != "bsc" and course != "msci": # weeding out fake courses
             message = message_start + "That's not a real course :(" # preparing to tell the user to go away
             await interaction.response.send_message(message) # telling the user to go away
         else: # if the course is real
             failure = False # it hasn't failed yet
-            
+
             try: # seeing if it will fail in a safe way
                 year = int(year) # checking the year is an int
                 value = float(value) # checking the % is valid
@@ -58,15 +85,14 @@ class EverestCommandCog(TeXBotBaseCog):
                         else: # way
                             year_value = 0.75 # to
                     if course == "msci": # do
-                        if year == 1: # a 
+                        if year == 1: # a
                             year_value = 0 # dictionary
                         elif year == 2: # but
-                            year_value = 0.2 # too 
+                            year_value = 0.2 # too
                         else: # bad
                             year_value = 0.4 # how much is each year worth
-                            
+
                     steps = (value / 100) * 1 / 6 * year_value * 44250 # actually calculating the steps of everest (assumes modules are worth 20 / 120 credits)
 
                     message = message_start + "This assignment is worth " + str(int(steps)) + " steps of Mt. Everest!" # preparing to tell the user how much they walked
                     await interaction.response.send_message(message) # telling the user how much they walked
-
