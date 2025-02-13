e28e821d5edea7ca5ca78aa834db3f555beaa8b4
Matt Norton <matt@carrotmanmatt.com>
2025-02-13 09:16:30 +0000

    Revert "Add deferral and member_count command" (#417)
    Revert "Add deferral to `makemember` and add member_count command (#412)"
    
    This reverts commit 64040a08fbf8d875f5cc5004010f47c78c9f96e5.

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -29,7 +29,7 @@ from .induct import (
 )
 from .kill import KillCommandCog
 from .make_applicant import MakeApplicantContextCommandsCog, MakeApplicantSlashCommandCog
-from .make_member import MakeMemberCommandCog, MemberCountCommandCog
+from .make_member import MakeMemberCommandCog
 from .ping import PingCommandCog
 from .remind_me import ClearRemindersBacklogTaskCog, RemindMeCommandCog
 from .send_get_roles_reminders import SendGetRolesRemindersTaskCog
@@ -66,7 +66,6 @@ __all__: "Sequence[str]" = (
     "MakeApplicantSlashCommandCog",
     "MakeMemberCommandCog",
     "ManualModerationCog",
-    "MemberCountCommandCog",
     "PingCommandCog",
     "RemindMeCommandCog",
     "SendGetRolesRemindersTaskCog",
@@ -104,7 +103,6 @@ def setup(bot: "TeXBot") -> None:
         MakeApplicantSlashCommandCog,
         MakeMemberCommandCog,
         ManualModerationCog,
-        MemberCountCommandCog,
         PingCommandCog,
         RemindMeCommandCog,
         SendGetRolesRemindersTaskCog,
