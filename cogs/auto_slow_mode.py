867e775ef57501db5d59fc0a6894d95af31b17b6
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2025-05-18 22:33:56 +0100

    Implement the task

--- cogs/auto_slow_mode.py
+++ cogs/auto_slow_mode.py
@@ -1,22 +1,25 @@
 """Module to handle automatic slow mode for Discord channels."""
 
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override
 
 import discord
+from discord.ext import tasks
 
 from config import settings
 from utils import CommandChecks, TeXBotBaseCog
+from utils.error_capture_decorators import capture_guild_does_not_exist_error
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
     from logging import Logger
     from typing import Final
 
-    from utils import TeXBotApplicationContext
+    from utils import TeXBot, TeXBotApplicationContext
 
 
 __all__: "Sequence[str]" = ("AutomaticSlowModeCommandCog",)
+
 
 logger: "Final[Logger]" = logging.getLogger("TeX-Bot")
 
@@ -24,11 +27,38 @@ logger: "Final[Logger]" = logging.getLogger("TeX-Bot")
 class AutomaticSlowModeBaseCog(TeXBotBaseCog):
     """Base class for automatic slow mode functionality."""
 
-    async def calculate_message_rate(
-        self, ctx: "TeXBotApplicationContext", channel: discord.TextChannel
-    ) -> None:
+    async def calculate_message_rate(self, channel: discord.TextChannel) -> int:
         """Calculate the message rate for a given channel."""
         raise NotImplementedError
+
+
+class AutomaticSlowModeTaskCog(AutomaticSlowModeBaseCog):
+    """Task to handle automatic slow mode for Discord channels."""
+
+    @override
+    def __init__(self, bot: "TeXBot") -> None:
+        """Start all task managers when this cog is initialised."""
+        if settings["AUTO_SLOW_MODE"]:
+            _ = self.auto_slow_mode_task.start()
+
+        super().__init__(bot)
+
+    @override
+    def cog_unload(self) -> None:
+        """
+        Unload-hook that ends all running tasks whenever the cog is unloaded.
+
+        This may be run dynamically or when the bot closes.
+        """
+        self.auto_slow_mode_task.cancel()
+
+    @tasks.loop(seconds=60)
+    @capture_guild_does_not_exist_error
+    async def auto_slow_mode_task(self) -> None:
+        """Task to automatically adjust slow mode in channels."""
+        for channel in self.bot.get_all_channels():
+            if isinstance(channel, discord.TextChannel):
+                await self.calculate_message_rate(channel)
 
 
 class AutomaticSlowModeCommandCog(AutomaticSlowModeBaseCog):
