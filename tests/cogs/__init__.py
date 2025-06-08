6ddc542a128c87ebfe29702d2c184c63dbab9a9a
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2025-06-08 21:28:39 +0000

    Initial setup

---
+++ tests/cogs/__init__.py
@@ -0,0 +1,11 @@
+"""This module is used to initialize the tests.cogs package."""
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+__all__: "Sequence[str]" = ()
+
+
+
---
+++ tests/cogs/test_induct.py
@@ -0,0 +1,15 @@
+"""Module to test the induct command."""
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+__all__: "Sequence[str]" = ()
+
+
+
+# TODO: random welcome message
+
+
+
