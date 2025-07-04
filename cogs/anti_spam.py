62000a5403e846b0ccbaac4d10ab009f600d6004
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2025-07-04 11:14:17 +0100

    Fix some stuff

--- cogs/anti_spam.py
+++ cogs/anti_spam.py
@@ -1,10 +1,12 @@
 """Module for the anti-spam functionality of the bot."""
 
 from typing import TYPE_CHECKING
-from utils import TeXBotBaseCog
+
 import discord
 
-if TYPE_CHECKING
+from utils import TeXBotBaseCog
+
+if TYPE_CHECKING:
     from collections.abc import Sequence
 
 
@@ -16,7 +18,7 @@ class AntiSpamCog(TeXBotBaseCog):
 
     @TeXBotBaseCog.listener()
     async def on_message(self, message: discord.Message) -> None:
-        author: discord.Member = message.author
+        author: discord.Member | discord.User = message.author
 
         if author.bot:
             return
@@ -25,7 +27,4 @@ class AntiSpamCog(TeXBotBaseCog):
 
         if len(message_history) < 2:
             return
-        
-        
-
 
