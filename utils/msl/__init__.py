44d82152d85961ba3b8ba7c698655bb4679b450c
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2025-10-07 17:42:25 +0100

    Fix mypy

--- utils/msl/__init__.py
+++ utils/msl/__init__.py
@@ -3,16 +3,20 @@
 from typing import TYPE_CHECKING
 
 from .authorisation import get_su_platform_access_cookie_status, get_su_platform_organisations
-from .memberships import get_full_membership_list, get_membership_count, is_student_id_member
+from .memberships import (
+    fetch_community_group_members_count,
+    fetch_community_group_members_list,
+    is_id_a_community_group_member,
+)
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
 __all__: "Sequence[str]" = (
     "GLOBAL_SSL_CONTEXT",
-    "get_full_membership_list",
-    "get_membership_count",
+    "fetch_community_group_members_count",
+    "fetch_community_group_members_list",
     "get_su_platform_access_cookie_status",
     "get_su_platform_organisations",
-    "is_student_id_member",
+    "is_id_a_community_group_member",
 )
