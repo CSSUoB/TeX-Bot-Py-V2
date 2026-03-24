3f3be73d7835ead2d8e9805478ae9bf44b66064b
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2026-03-24 00:27:36 +0000

    Start working on auto report

---
+++ cogs/moderation.py
@@ -0,0 +1,132 @@
+"""File to hold cog classes related to moderation actions and tracking."""
+
+import logging
+from typing import TYPE_CHECKING
+
+import discord
+
+from utils import TeXBotBaseCog
+from utils.error_capture_decorators import capture_guild_does_not_exist_error
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from logging import Logger
+    from typing import Final
+
+
+__all__: "Sequence[str]" = ("ModerationCog",)
+
+
+logger: "Final[Logger]" = logging.getLogger("TeX-Bot")
+
+
+class ModerationCog(TeXBotBaseCog):
+
+    most_recently_deleted_message: discord.Message | None = None
+
+    async def _send_message_to_committee(
+        self, message: discord.Message, deleter: discord.Member
+    ) -> None:
+        discord_channel: discord.TextChannel | None = discord.utils.get(
+            self.bot.main_guild.text_channels,
+            name="discord",  # TODO: Make this user-configurable  # noqa: FIX002
+        )
+
+        if not discord_channel:
+            logger.error("Could not find the channel to send the message deletion report to!")
+            return
+
+        embed_content: str = ""
+
+        if message.content:
+            embed_content += message.content[:600]
+            if len(message.content) > 600:
+                embed_content += " _... (truncated to 600 characters)_"
+        else:
+            embed_content += "_Deleted message had no content_"
+            if len(message.attachments) > 0 or len(message.embeds) > 0:
+                embed_content += " _but did have one or more attachments!_"
+
+        embed_content += f"\n[View Original]({message.jump_url})"
+
+        if message.reference:
+            embed_content += f"\n[View Message this replied to]({message.reference.jump_url})"
+
+        message_author_avatar_url: str | None = message.author.display_avatar.url
+
+        embed_author: discord.EmbedAuthor = discord.EmbedAuthor(
+            name=message.author.display_name, icon_url=message_author_avatar_url
+        )
+
+        embed_image: str | None = None
+        if len(message.attachments) == 1:
+            attachment_type: str | None = message.attachments[0].content_type
+            if attachment_type and "image" in attachment_type:
+                embed_image = message.attachments[0].url
+
+        await discord_channel.send(
+            content=(
+                f"{deleter.mention} deleted a message from {message.author.mention} "
+                f"in {
+                    message.channel.mention
+                    if isinstance(
+                        message.channel,
+                        (
+                            discord.TextChannel,
+                            discord.VoiceChannel,
+                            discord.StageChannel,
+                            discord.Thread,
+                        ),
+                    )
+                    else message.channel
+                }:"
+            ),
+            embed=discord.Embed(
+                author=embed_author,
+                description=embed_content,
+                colour=message.author.colour,
+                image=embed_image,
+            ),
+        )
+
+    @TeXBotBaseCog.listener()
+    @capture_guild_does_not_exist_error
+    async def on_message_delete(self, message: discord.Message) -> None:
+        """Listen for message deletions."""
+        self.most_recently_deleted_message = message
+
+    @TeXBotBaseCog.listener()
+    @capture_guild_does_not_exist_error
+    async def on_audit_log_entry(self, entry: discord.AuditLogEntry) -> None:
+        """Listen for audit log entries."""
+        if entry.action != discord.AuditLogAction.message_delete or not self.most_recently_deleted_message:
+            return
+
+        deleter: discord.Member | discord.User | None = entry.user
+        author: discord.User | discord.Member | None = entry.target
+        channel: discord.TextChannel | None = entry.extra.channel
+
+        if (
+            not isinstance(channel, discord.TextChannel)
+            or not isinstance(author, discord.Member)
+            or not isinstance(deleter, discord.Member)
+        ):
+            return
+
+        if author != self.most_recently_deleted_message.author:
+            return
+
+        author_name = author.name
+        author_id = author.id
+        channel_name = channel.name
+        deleter_name = deleter.name
+        deleter_id = deleter.id
+
+        logger.error(
+            "Message by %s (ID: %s) in #%s was deleted by %s (ID: %s).",
+            author_name,
+            author_id,
+            channel_name,
+            deleter_name,
+            deleter_id,
+        )
