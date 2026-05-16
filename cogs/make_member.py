241a5bce7ce6f5df7fd66a8d9c53a2e6754ca72f
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2026-05-16 13:24:04 +0100

    Maybe working

--- cogs/make_member.py
+++ cogs/make_member.py
@@ -11,7 +11,7 @@ from django.core.exceptions import ValidationError
 from config import settings
 from db.core.models import GroupMadeMember
 from exceptions import ApplicantRoleDoesNotExistError, GuestRoleDoesNotExistError
-from utils import CommandChecks, TeXBotApplicationContext, TeXBotBaseCog
+from utils import CommandChecks, TeXBotBaseCog
 from utils.msl import (
     fetch_community_group_members_count,
     is_id_a_community_group_member,
@@ -21,6 +21,8 @@ if TYPE_CHECKING:
     from collections.abc import Sequence
     from logging import Logger
     from typing import Final
+
+    from utils import TeXBotApplicationContext
 
 __all__: "Sequence[str]" = (
     "MakeMemberCommandCog",
@@ -56,7 +58,99 @@ _GROUP_MEMBER_ID_ARGUMENT_NAME: "Final[str]" = (
 )
 
 
-class MakeMemberCommandCog(TeXBotBaseCog):
+class MakeMemberBaseCog(TeXBotBaseCog):
+    """Base cog class for make member interactions."""
+
+    async def _perform_make_member(
+        self, user: discord.User | discord.Member, raw_group_member_id: str
+    ) -> tuple[bool, str]:
+        """Perform the actions to make a user a member."""
+        member_role: discord.Role = await self.bot.member_role
+        discord_member: discord.Member = await self.bot.get_main_guild_member(user)
+
+        INVALID_GROUP_MEMBER_ID_MESSAGE: Final[str] = (
+            f"{raw_group_member_id!r} is not a valid {self.bot.group_member_id_type} ID."
+        )
+
+        if not re.fullmatch(r"\A\d{7}\Z", raw_group_member_id):
+            return False, INVALID_GROUP_MEMBER_ID_MESSAGE
+
+        try:
+            group_member_id: int = int(raw_group_member_id)
+        except ValueError:
+            return False, INVALID_GROUP_MEMBER_ID_MESSAGE
+
+        if member_role in discord_member.roles:
+            return (False, (
+                ":information_source: No changes made. "
+                "You're already a member - why are you trying this again? :information_source:"
+            ))
+
+        if await GroupMadeMember.objects.filter(
+            hashed_group_member_id=GroupMadeMember.hash_group_member_id(
+                group_member_id, self.bot.group_member_id_type
+            )
+        ).aexists():
+            return False, "This student ID has already been used."
+
+        if not await is_id_a_community_group_member(member_id=group_member_id):
+            return False, (
+                f"You must be a member of {self.bot.group_full_name} "
+                "to use this command.\n"
+                f"The provided {_GROUP_MEMBER_ID_ARGUMENT_NAME} must match "
+                f"the {self.bot.group_member_id_type} ID "
+                f"that you purchased your {self.bot.group_short_name} membership with."
+            )
+
+        await discord_member.add_roles(
+            member_role, reason=f"{discord_member} used TeX-Bot to become a member"
+        )
+
+        try:
+            await GroupMadeMember.objects.acreate(group_member_id=raw_group_member_id)  # type: ignore[misc]
+        except ValidationError as create_group_made_member_error:
+            error_is_already_exists: bool = (
+                "hashed_group_member_id" in create_group_made_member_error.message_dict
+                and any(
+                    "already exists" in error
+                    for error in create_group_made_member_error.message_dict[
+                        "hashed_group_member_id"
+                    ]
+                )
+            )
+            if not error_is_already_exists:
+                raise
+
+            try:
+                guest_role: discord.Role = await self.bot.guest_role
+            except GuestRoleDoesNotExistError:
+                logger.warning(
+                    '"/make-member" command used but the "Guest" role does not exist. '
+                    'Some user\'s may now have the "Member" role without the "Guest" role. '
+                    'Use the "/ensure-members-inducted" command to fix this issue.'
+                )
+            else:
+                if guest_role not in discord_member.roles:
+                    await discord_member.add_roles(
+                        guest_role,
+                        reason=f"{discord_member} used TeX-Bot to become a member.",
+                    )
+
+            try:
+                applicant_role: discord.Role = await self.bot.applicant_role
+            except ApplicantRoleDoesNotExistError:
+                pass
+            else:
+                if applicant_role in discord_member.roles:
+                    await discord_member.remove_roles(
+                        applicant_role,
+                        reason=f"{discord_member} used TeX-Bot to become a member.",
+                    )
+
+        return True, ""
+
+
+class MakeMemberCommandCog(MakeMemberBaseCog):
     """Cog class that defines the "/make-member" command and its call-back method."""
 
     @discord.slash_command(
@@ -105,112 +199,14 @@ class MakeMemberCommandCog(TeXBotBaseCog):
         has purchased a valid membership to your community group,
         then gives the member the "Member" role.
         """
-        # NOTE: Shortcut accessors are placed at the top of the function so that the exceptions they raise are displayed before any further errors may be sent
-        member_role: discord.Role = await self.bot.member_role
-        interaction_member: discord.Member = await ctx.bot.get_main_guild_member(ctx.user)
-
-        INVALID_GROUP_MEMBER_ID_MESSAGE: Final[str] = (
-            f"{raw_group_member_id!r} is not a valid {self.bot.group_member_id_type} ID."
-        )
-
-        if not re.fullmatch(r"\A\d{7}\Z", raw_group_member_id):
-            await self.command_send_error(ctx, message=(INVALID_GROUP_MEMBER_ID_MESSAGE))
-            return
-
-        try:
-            group_member_id: int = int(raw_group_member_id)
-        except ValueError:
-            await self.command_send_error(ctx, message=INVALID_GROUP_MEMBER_ID_MESSAGE)
-            return
-
         await ctx.defer(ephemeral=True)
-        async with ctx.typing():
-            if member_role in interaction_member.roles:
-                await ctx.followup.send(
-                    content=(
-                        ":information_source: No changes made. You're already a member "
-                        "- why are you trying this again? :information_source:"
-                    ),
-                    ephemeral=True,
-                )
-                return
 
-            if await GroupMadeMember.objects.filter(
-                hashed_group_member_id=GroupMadeMember.hash_group_member_id(
-                    group_member_id, self.bot.group_member_id_type
-                )
-            ).aexists():
-                await ctx.followup.send(
-                    content=(
-                        ":information_source: No changes made. This student ID has already "
-                        f"been used. Please contact a {
-                            await self.bot.get_mention_string(self.bot.committee_role)
-                        } member if this is an error. :information_source:"
-                    ),
-                    ephemeral=True,
-                )
-                return
-
-            if not await is_id_a_community_group_member(member_id=group_member_id):
-                await self.command_send_error(
-                    ctx,
-                    message=(
-                        f"You must be a member of {self.bot.group_full_name} "
-                        "to use this command.\n"
-                        f"The provided {_GROUP_MEMBER_ID_ARGUMENT_NAME} must match "
-                        f"the {self.bot.group_member_id_type} ID "
-                        f"that you purchased your {self.bot.group_short_name} membership with."
-                    ),
-                )
-                return
-
-            # NOTE: The "Member" role must be added to the user **before** the "Guest" role to ensure that the welcome message does not include the suggestion to purchase membership
-            await interaction_member.add_roles(
-                member_role, reason=f'{ctx.user} used TeX Bot slash-command: "/make-member"'
+        with ctx.typing():
+            _, message = await self._perform_make_member(
+                user=ctx.user, raw_group_member_id=raw_group_member_id
             )
 
-            try:
-                await GroupMadeMember.objects.acreate(group_member_id=raw_group_member_id)  # type: ignore[misc]
-            except ValidationError as create_group_made_member_error:
-                error_is_already_exists: bool = (
-                    "hashed_group_member_id" in create_group_made_member_error.message_dict
-                    and any(
-                        "already exists" in error
-                        for error in create_group_made_member_error.message_dict[
-                            "hashed_group_member_id"
-                        ]
-                    )
-                )
-                if not error_is_already_exists:
-                    raise
-
-            await ctx.followup.send(content="Successfully made you a member!", ephemeral=True)
-
-            try:
-                guest_role: discord.Role = await self.bot.guest_role
-            except GuestRoleDoesNotExistError:
-                logger.warning(
-                    '"/make-member" command used but the "Guest" role does not exist. '
-                    'Some user\'s may now have the "Member" role without the "Guest" role. '
-                    'Use the "/ensure-members-inducted" command to fix this issue.'
-                )
-            else:
-                if guest_role not in interaction_member.roles:
-                    await interaction_member.add_roles(
-                        guest_role,
-                        reason=f'{ctx.user} used TeX Bot slash-command: "/make-member"',
-                    )
-            applicant_role: discord.Role | None
-            try:
-                applicant_role = await ctx.bot.applicant_role
-            except ApplicantRoleDoesNotExistError:
-                applicant_role = None
-
-            if applicant_role and applicant_role in interaction_member.roles:
-                await interaction_member.remove_roles(
-                    applicant_role,
-                    reason=f'{ctx.user} used TeX Bot slash-command: "/make-member"',
-                )
+            await ctx.followup.send(content=message, ephemeral=True)
 
 
 class MemberCountCommandCog(TeXBotBaseCog):
@@ -232,7 +228,7 @@ class MemberCountCommandCog(TeXBotBaseCog):
             )
 
 
-class MakeMemberModalActual(Modal):
+class MakeMemberModalActual(Modal, MakeMemberBaseCog):
     """A discord.Modal containing a the input box for make member user interaction."""
 
     @override
@@ -257,25 +253,24 @@ class MakeMemberModalActual(Modal):
             )
             return
 
-        try:
-            student_id: int = int(raw_student_id)
-        except ValueError:
+        if not interaction.user:
             await interaction.response.send_message(
-                content="Student ID must be a number.", ephemeral=True
+                content="Something went wrong, contact a committee member if this persists.",
+                ephemeral=True,
+            )
+            logger.debug(
+                "Interaction user was unexpectedly None in MakeMemberModal. Interaction: %s",
+                interaction.data,
             )
             return
 
-        if await is_id_a_community_group_member(member_id=student_id):
-            await MakeMemberModalCommandCog.give_member_role(
-                self=MakeMemberModalCommandCog(bot=interaction.client),  # type: ignore[arg-type]
-                interaction=interaction,
-            )
-            await interaction.response.send_message(content="Action complete.")
-            return
+        await interaction.response.defer(ephemeral=True)
 
-        await interaction.response.send_message(
-            content="Student ID not found.", ephemeral=True
+        _, message = await self._perform_make_member(
+            user=interaction.user, raw_group_member_id=raw_student_id
         )
+
+        await interaction.followup.send(content=message, ephemeral=True)
 
 
 class OpenMemberVerifyModalView(View):
@@ -293,27 +288,13 @@ class OpenMemberVerifyModalView(View):
         await interaction.response.send_modal(MakeMemberModalActual())
 
 
-class MakeMemberModalCommandCog(TeXBotBaseCog):
+class MakeMemberModalCommandCog(MakeMemberBaseCog):
     """Cog class that defines the "/make-member-modal" command and its call-back method."""
 
     @TeXBotBaseCog.listener()
     async def on_ready(self) -> None:
         """Add OpenMemberVerifyModalView to the bot's list of permanent views."""
         self.bot.add_view(OpenMemberVerifyModalView())
-
-    async def give_member_role(self, interaction: discord.Interaction) -> None:
-        """Give the member role to the user who interacted with the modal."""
-        if not isinstance(interaction.user, discord.Member):
-            await self.command_send_error(
-                ctx=TeXBotApplicationContext(bot=interaction.client, interaction=interaction),  # type: ignore[arg-type]
-                message="User is not a member.",
-            )
-            return
-
-        await interaction.user.add_roles(
-            await self.bot.member_role,
-            reason=f'{interaction.user} used TeX Bot modal: "Make Member"',
-        )
 
     async def _open_make_new_member_modal(
         self,
