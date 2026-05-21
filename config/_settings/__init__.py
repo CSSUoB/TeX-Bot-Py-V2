ff1939a86de3da33efe3bea7322e3cdb964103a2
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2026-05-21 20:49:05 +0100

    Stuff

--- config/_settings/__init__.py
+++ config/_settings/__init__.py
@@ -5,10 +5,15 @@ Settings values are imported from the tex-bot-deployment.yaml file.
 These values are used to configure the functionality of the bot at run-time.
 """
 
+import datetime
 import logging
+import re
 from typing import TYPE_CHECKING
 
+import utils
+
 if TYPE_CHECKING:
+    from collections.abc import Sequence
     from logging import Logger
     from typing import ClassVar, Final
 
@@ -42,11 +47,59 @@ class SettingsAccessor:
         """
         return
 
-    @classmethod
-    async def set_setting_value(cls, config_setting_name: str, value: object) -> None:
-        """
-        Set the specified setting to the given value.
+    def __getattr__(self, item: str) -> object:
+        """Retrieve settings value by attribute lookup."""
+        MISSING_ATTRIBUTE_MESSAGE: Final[str] = (
+            f"{type(self).__name__!r} object has no attribute {item!r}"
+        )
 
-        If the setting does not exist, it will be created.
-        """
-        return
+        if "_pytest" in item or item in ("__bases__", "__test__"):  # NOTE: Overriding __getattr__() leads to many edge-case issues where external libraries will attempt to call getattr() with peculiar values
+            raise AttributeError(MISSING_ATTRIBUTE_MESSAGE)
+
+        IN_SETTING_KEY_FORMAT: Final[bool] = bool(
+            re.fullmatch(r"\A(?!.*__.*)(?:[A-Z]|[A-Z_][A-Z]|[A-Z_][A-Z][A-Z_]*[A-Z])\Z", item)
+        )
+        if not IN_SETTING_KEY_FORMAT:
+            raise AttributeError(MISSING_ATTRIBUTE_MESSAGE)
+
+        if self._most_recent_yaml is None:
+            YAML_NOT_LOADED_MESSAGE: Final[str] = (
+                "Configuration cannot be accessed before it is loaded."
+            )
+            raise RuntimeError(YAML_NOT_LOADED_MESSAGE)
+
+        if item not in self._settings:
+            INVALID_SETTINGS_KEY_MESSAGE: Final[str] = self._get_invalid_settings_key_message(
+                item,
+            )
+            raise AttributeError(INVALID_SETTINGS_KEY_MESSAGE)
+
+        ATTEMPTING_TO_ACCESS_BOT_TOKEN_WHEN_ALREADY_RUNNING: Final[bool] = bool(
+            "bot" in item.lower() and "token" in item.lower() and utils.is_running_in_async()
+        )
+        if ATTEMPTING_TO_ACCESS_BOT_TOKEN_WHEN_ALREADY_RUNNING:
+            TEX_BOT_ALREADY_RUNNING_MESSAGE: Final[str] = (
+                f"Cannot access {item!r} when TeX-Bot is already running."
+            )
+            raise RuntimeError(TEX_BOT_ALREADY_RUNNING_MESSAGE)
+
+        return self._settings[item]
+
+    def __getitem__(self, item: str) -> object:
+        """Retrieve settings value by key lookup."""
+        attribute_not_exist_error: AttributeError
+        try:
+            return getattr(self, item)
+        except AttributeError as attribute_not_exist_error:
+            key_error_message: str = item
+
+            ERROR_WAS_FROM_INVALID_KEY_NAME: Final[bool] = (
+                self._get_invalid_settings_key_message(item) in str(
+                    attribute_not_exist_error,
+                )
+            )
+            if ERROR_WAS_FROM_INVALID_KEY_NAME:
+                key_error_message = str(attribute_not_exist_error)
+
+            raise KeyError(key_error_message) from None
+
