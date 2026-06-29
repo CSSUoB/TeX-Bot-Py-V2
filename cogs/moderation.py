0eebf8fbb5735be82ed23f704efd9a9c77d02032
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2026-06-29 21:16:20 +0100

    Remove duplication

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -25,71 +25,6 @@ class ModerationCog(TeXBotBaseCog):
 
     most_recently_deleted_message: discord.Message | None = None
 
-    async def _send_message_to_committee(
-        self, message: discord.Message, deleter: discord.Member
-    ) -> None:
-        discord_channel: discord.TextChannel | None = discord.utils.get(
-            self.bot.main_guild.text_channels,
-            name="discord",  # TODO: Make this user-configurable  # noqa: FIX002
-        )
-
-        if not discord_channel:
-            logger.error("Could not find the channel to send the message deletion report to!")
-            return
-
-        embed_content: str = ""
-
-        if message.content:
-            embed_content += message.content[:600]
-            if len(message.content) > 600:
-                embed_content += " _... (truncated to 600 characters)_"
-        else:
-            embed_content += "_Deleted message had no content_"
-            if len(message.attachments) > 0 or len(message.embeds) > 0:
-                embed_content += " _but did have one or more attachments!_"
-
-        embed_content += f"\n[View Original]({message.jump_url})"
-
-        if message.reference:
-            embed_content += f"\n[View Message this replied to]({message.reference.jump_url})"
-
-        message_author_avatar_url: str | None = message.author.display_avatar.url
-
-        embed_author: discord.EmbedAuthor = discord.EmbedAuthor(
-            name=message.author.display_name, icon_url=message_author_avatar_url
-        )
-
-        embed_image: str | None = None
-        if len(message.attachments) == 1:
-            attachment_type: str | None = message.attachments[0].content_type
-            if attachment_type and "image" in attachment_type:
-                embed_image = message.attachments[0].url
-
-        await discord_channel.send(
-            content=(
-                f"{deleter.mention} deleted a message from {message.author.mention} "
-                f"in {
-                    message.channel.mention
-                    if isinstance(
-                        message.channel,
-                        (
-                            discord.TextChannel,
-                            discord.VoiceChannel,
-                            discord.StageChannel,
-                            discord.Thread,
-                        ),
-                    )
-                    else message.channel
-                }:"
-            ),
-            embed=discord.Embed(
-                author=embed_author,
-                description=embed_content,
-                colour=message.author.colour,
-                image=embed_image,
-            ),
-        )
-
     @TeXBotBaseCog.listener()
     async def on_message_delete(self, message: discord.Message) -> None:
         """Listen for message deletions."""
@@ -128,6 +63,6 @@ class ModerationCog(TeXBotBaseCog):
         ):
             return
 
-        await self._send_message_to_committee(self.most_recently_deleted_message, deleter)
+        await self.send_message_to_committee(self.most_recently_deleted_message, deleter)
 
         self.most_recently_deleted_message = None
